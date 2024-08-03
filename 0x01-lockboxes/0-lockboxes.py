9928e0eaf09433f79db42f8641bec19c577b778a
dahunsi-dami <gbemigadare@gmail.com>
2024-08-03 16:20:52 +0100

    Use DFS(stack) to check if all boxes can be opened.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""Module with method to tell if all boxes can be opened."""
 
+
+def canUnlockAll(boxes):
+    """Method to determine if all boxes can be opened."""
+
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    stack = [0]
+
+    while stack:
+        box_num = stack.pop()
+        for i in boxes[box_num]:
+            if 0 <= i < n and not unlocked[i]:
+                unlocked[i] = True
+                stack.append(i)
+
+    return all(unlocked)
