e5a9b099a8680af140b9d55aff610e886f7c6be3
dahunsi-dami <gbemigadare@gmail.com>
2024-09-23 23:43:57 +0100

    Solve the coin change problem w/ greedy algo instead.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,10 +3,30 @@
 
 
 def makeChange(coins, total):
-    """Calculates fewest coins needed to make total."""
+    """
+    Calculates fewest coins needed to make total
+    using greedy algo.
+    """
     if total <= 0:
         return 0
 
+    coins.sort(reverse=True)
+
+    coin_kaunt = 0
+    remnant = total
+
+    for coin in coins:
+        if remnant >= coin:
+            kaunt = remnant // coin
+            coin_kaunt += kaunt
+            remnant -= kaunt * coin
+
+        if remnant == 0:
+            return coin_kaunt
+
+    return -1 if remnant > 0 else coin_kaunt
+
+    """
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
@@ -15,3 +35,4 @@ def makeChange(coins, total):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
+    """
