17081a4a1f843fd44e51ab5e1b4b4a8b2e19ae01
dahunsi-dami <gbemigadare@gmail.com>
2024-10-23 15:45:17 +0100

    Calculate fewest no. of ops to result in n H chars in given file.

---
+++ 0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Minimum operations implementation interview task."""
+
+
+def minOperations(n: int) -> int:
+    """
+    Calculates fewest number of operations needed-
+    -to result in exactly n H characters in the file.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
